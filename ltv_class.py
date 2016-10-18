bdeec6c39a336fe113aa7f84303dbf2a591fb9aa
Goren Gordon <goren@gorengordon.dom>
2016-10-17 22:59:26 -0400

    Probability code works. Ranking code works.

--- ltv_class.py
+++ ltv_class.py
@@ -2,6 +2,11 @@ import csv
 import numpy as np
 import copy
 from operator import mul
+import re
+
+period_names = {'week': 7, 'month': 30, '3months': 90}
+data_path = '../../LTV/data/'
+reports_path = '../../LTV/data/ltv/'
 
 
 def get_number_of_csv_lines(filename):
@@ -12,17 +17,14 @@ def get_number_of_csv_lines(filename):
             num_lines += 1
     return num_lines
 
-data_path = '../../LTV/data/'
-period_names = {'week': 7, 'month': 30}
-
 
 class LtvClass:
 
-    def __init__(self, filename, max_count=1000, target_periods=None, max_users=1000):
+    def __init__(self, filename, max_count=1000, target_periods=None):
         self.filename = filename
         self.max_count = max_count
-        self.max_users = max_users
-        self.n_bins = 20  # number of bins in distribution
+        self.max_users = 1000
+        self.n_bins = 100  # number of bins in distribution
 
         self.prob = {}  # prob: key=id, value = []_num_target_periods = numpy p(amount)
         self.priors = None  # numpy[t, a]
@@ -37,7 +39,6 @@ class LtvClass:
             self.num_target_periods = len(target_periods)
             self.target_periods = target_periods
         self.value_stats = []
-
         self.report_data = []
 
         self.user_data = {}
@@ -86,7 +87,8 @@ class LtvClass:
         self.n_bins = n_bins
         self.priors = []
         for i in range(self.num_target_periods):
-            self.priors.append(np.zeros([self.n_bins]))
+            minimum_prior = 1.0 / float(self.value_stats[i]['count'] + self.n_bins)
+            self.priors.append(np.ones([self.n_bins]) * minimum_prior)
 
     def convert_value_to_bin(self, value, value_stats):
         # log-distribution
@@ -117,20 +119,27 @@ class LtvClass:
                         try:
                             i = self.target_periods.index(self.data_target_periods[e])
                             v_bin = self.convert_value_to_bin(v, self.value_stats[i])
-                            self.priors[i][v_bin] += 1.0 / self.value_stats[i]['count']
+                            self.priors[i][v_bin] += 1.0 / float(self.value_stats[i]['count'] + self.n_bins)
                         except:
                             pass
                 row_count += 1
                 if row_count >= self.max_count:
                     break
+        for i, p in enumerate(self.priors):
+            p_value = self.get_value_from_prob(p,i)
+
+    # ========= probability ============================
 
     def get_reports_data(self, report_filenames):
         self.report_data = []
         for rf in report_filenames:
-            s = rf.split('_')
-            period_index = self.target_periods.index(period_names[s[2]])
-            more_less = s[1][:4] == 'more'
-            report_value = float(s[1][4:])
+            m = re.search('coming(.+?).csv', rf)
+            period_index = self.target_periods.index(period_names[m.group(1)])
+            more_less = 'more' in rf
+            m = re.search('than(.+?)coming', rf)
+            report_value = float(m.group(1))
+            FTB_Users = 'FTB' in rf
+
             prob_less_in_prior = self.get_prob_of_less_than_value(period_index, self.priors[period_index], report_value)
             if more_less:
                 query_prob = 1 - prob_less_in_prior
@@ -138,69 +147,103 @@ class LtvClass:
                 query_prob = prob_less_in_prior
             self.report_data.append({
                 'filename': rf,
-                'type': s[0],
+                'FTB_Users': FTB_Users,
                 'period_index': period_index,
                 'more_less': more_less,
                 'report_value': report_value,
                 'prob_in_prior': query_prob
             })
 
-    def get_users(self):
+    def get_users(self, max_users=1000):
+        self.max_users = max_users
         self.user_data = {}
         for rd in self.report_data:
-            with open(data_path + 'reports/' + rd['filename'] + '.csv', 'rb') as csv_file:
+            with open(reports_path + rd['filename'], 'rb') as csv_file:
                 report_file = csv.reader(csv_file, delimiter=',')
                 for row in report_file:
                     user_id = row[0]
                     if user_id not in self.user_data:
                         self.user_data[user_id] = {
                             'prob': copy.deepcopy(self.priors),
-                            'ltv': np.zeros([self.num_target_periods])
+                            'ltv': np.zeros([self.num_target_periods]),
+                            'rank_prediction': []
                         }
+                        for i in self.target_periods:
+                            self.user_data[user_id]['rank_prediction'].append([])
 
                         if self.max_users > 0 and len(self.user_data) > self.max_users:
                             return
+        print('users: ', len(self.user_data))
 
     def update_posteriors(self):
         for rd in self.report_data:
-            with open(data_path + 'reports/' + rd['filename'] + '.csv', 'rb') as csv_file:
+            with open(reports_path + rd['filename'], 'rb') as csv_file:
+                print('processing ', rd['filename'], '...')
                 report_file = csv.reader(csv_file, delimiter=',')
+                row_counter = 0
+                errors = 0
                 for row in report_file:
-                    user_id = row[0]
-                    the_prob = 0.5 #float(row[2])  # missing ------------------------------------------------------
+                    if row_counter > 0:
+                        user_id = row[0]
+                        the_prob = float(row[2])
 
-                    self.the_update(user_id, the_prob, rd)
+                        if not self.the_update(user_id, the_prob, rd):
+                            errors += 1
+                    row_counter += 1
+                print('done ', row_counter, '(', errors, ' errors)')
 
     def the_update(self, user_id, the_prob, rd):
-        try:
-            this_prior = self.user_data[user_id]['prob'][rd['period_index']]
-        except:
+        # try:
+        this_prior = self.user_data[user_id]['prob'][rd['period_index']]
+        # except:
             # print('the user: ', user_id, ' is not in the first ', self.max_users, ' users found')
-            return
+            # return
 
         report_value = rd['report_value']
         report_value_bin = self.convert_value_to_bin(report_value, self.value_stats[rd['period_index']])
+
+        prior_mean_ltv = self.get_value_from_prob(this_prior, rd['period_index'])
 
         # Bayes
         the_posterior = copy.deepcopy(this_prior)
         for b, p in enumerate(the_posterior):
             if rd['more_less']:
                 if b > report_value_bin:
-                    p *= the_prob / rd['prob_in_prior']
+                    the_posterior[b] *= the_prob / rd['prob_in_prior']
                 else:
-                    p *= (1 - the_prob) / (1 - rd['prob_in_prior'])
+                    the_posterior[b] *= (1 - the_prob) / (1 - rd['prob_in_prior'])
             else:
                 if b < report_value_bin:
-                    p *= the_prob / rd['prob_in_prior']
+                    the_posterior[b] *= the_prob / rd['prob_in_prior']
                 else:
-                    p *= (1 - the_prob) / (1 - rd['prob_in_prior'])
+                    the_posterior[b] *= (1 - the_prob) / (1 - rd['prob_in_prior'])
 
-        self.user_data[user_id]['prob'][rd['period_index']] = the_posterior
+        posterior_mean_ltv = self.get_value_from_prob(the_posterior, rd['period_index'])
+        print('mean ltv:', prior_mean_ltv, posterior_mean_ltv)
+
+        # sanity checks!
+        sanity_checks = True
+        if LtvClass.get_sum_prob(this_prior) < 0.999:
+            # print('Error: prior does not sum to one: ', LtvClass.get_sum_prob(this_prior))
+            sanity_checks = False
+        if LtvClass.get_sum_prob(the_posterior) < 0.999:
+            # print('Error: posterior does not sum to one', LtvClass.get_sum_prob(this_prior), LtvClass.get_sum_prob(the_posterior))
+            sanity_checks = False
+        if LtvClass.get_sum_prob(np.abs(np.array(the_posterior) - np.array(this_prior))) < 0.001:
+            # print('Error: posterior did not update', LtvClass.get_sum_prob(np.abs(np.array(the_posterior) - np.array(this_prior))))
+            sanity_checks = False
+
+        sum_posterior = LtvClass.get_sum_prob(the_posterior)
+        for b in range(len(the_posterior)):
+            the_posterior[b] /= sum_posterior
+
+        self.user_data[user_id]['prob'][rd['period_index']] = copy.deepcopy(the_posterior)
+        return sanity_checks
 
     def get_prob_of_less_than_value(self, period_index, prior, value):
         value_bin = self.convert_value_to_bin(value, self.value_stats[period_index])
         prob_list = [prior[x] for x in range(value_bin)]
-        return np.prod(prob_list)
+        return np.sum(prob_list)
 
     def get_value_from_prob(self, prob, period_index):
         value = 0
@@ -209,7 +252,78 @@ class LtvClass:
         return value
 
     def get_ltv(self):
-        for u in self.user_data.values():
+        self.final_results = {}
+        for id, u in self.user_data.items():
+            self.final_results[id] = {'predicted': [], 'ground_truth': []}
             for i, prob in enumerate(u['prob']):
-                u['ltv'][i] = self.get_value_from_prob(prob, i)
+                self.final_results[id]['predicted'].append(self.get_value_from_prob(prob, i))
+                self.final_results[id]['ground_truth'].append(-1)
 
+    @staticmethod
+    def get_sum_prob(prob):
+        sum_prob = np.sum(prob)
+        return sum_prob
+
+    def check_result(self, filename):
+        with open(self.filename, 'rb') as csv_file:
+            file_reader = csv.reader(csv_file, delimiter=',')
+            row_count = 0
+            for row in file_reader:
+                if row_count > 0:
+                    user_id = row[0]
+                    values = [float(x) for x in row][1:]
+                    for e, v in enumerate(values):
+                        try:
+                            i = self.target_periods.index(self.data_target_periods[e])
+                            self.final_results[user_id]['ground_truth'][i] = v
+                        except:
+                            pass
+                row_count += 1
+
+    # ========= ranking ========================
+
+    def update_rankings(self):
+        for rd in self.report_data:
+            with open(reports_path + rd['filename'], 'rb') as csv_file:
+                report_file = csv.reader(csv_file, delimiter=',')
+                candidate_count = 0
+                for row in report_file:
+                    candidate_count += 1
+
+            cumulative_candidate = [0]
+            for p in self.priors[rd['period_index']]:
+                cumulative_candidate.append(cumulative_candidate[-1] + int(p * candidate_count))
+
+            with open(reports_path + rd['filename'], 'rb') as csv_file:
+                print('processing ', rd['filename'], '...')
+                report_file = csv.reader(csv_file, delimiter=',')
+                row_counter = 0
+                for row in report_file:
+                    if row_counter > 0:
+                        user_id = row[0]
+                        the_rank = float(row[1])
+
+                        self.the_update_rank(user_id, the_rank, cumulative_candidate, rd)
+                    row_counter += 1
+
+    def the_update_rank(self, user_id, the_rank, cumulative_candidate, rd):
+        for rank_bin, c in enumerate(cumulative_candidate):
+            if rank_bin > 0:
+                if rd['more_less']:     # if more, rank=1 --> last bin, rank=candidate_count --> first bin
+                    if the_rank > c:
+                        break
+                else:                   # if less, rank=1 --> first bin, rank=candidate_count --> last bin
+                    if the_rank < c:
+                        break
+        rank_bin -= 1
+
+        rank_based_value = self.convert_bin_to_value(rank_bin, self.value_stats[rd['period_index']])
+        self.user_data[user_id]['rank_prediction'][rd['period_index']].append(int(rank_based_value))
+
+    def get_ltv_ranking(self):
+        self.final_results = {}
+        for id, u in self.user_data.items():
+            self.final_results[id] = {'predicted': [], 'ground_truth': []}
+            for i, rank_prediction in enumerate(u['rank_prediction']):
+                self.final_results[id]['predicted'].append(np.median(rank_prediction))
+                self.final_results[id]['ground_truth'].append(-1)
--- main.py
+++ main.py
@@ -1,62 +1,89 @@
 from ltv_class import *
 import pickle
+from os import listdir
+from os.path import isfile, join
 
 process_flow = {
     'get_stats': True,
     'get_priors': True,
-    'update_posteriors': True,
-    'calc_ltv': True
+    'update_posteriors': False,
+    'calc_ltv': False,
+    'update_ranking': True,
+    'check_result': True,
+    'save_to_csv': True
 }
 
-filename = data_path + 'Ground_truth/ref-20160401.csv'
+
+past_filename = data_path + 'Ground_truth/ref-20160401.csv'
+present_filename = data_path + 'Ground_truth/ref-20160701.csv'
 results_path = '../../LTV/results/'
 
-if process_flow['get_stats']:
-    print('get_stats...')
-    the_class = LtvClass(filename=filename, target_periods=[7,30,90], max_count=10000, max_users=10000)
-    the_class.get_stats()
-    with open(results_path + 'ltv_class.pkl', 'wb') as output:
-        pickle.dump(the_class, output, pickle.HIGHEST_PROTOCOL)
+candidate_type = ['FTB', 'User']
+for c_type in candidate_type:
+    print('Starting ', c_type, '...')
+    report_filenames = [f for f in listdir(reports_path) if isfile(join(reports_path, f)) if 'less' in f and 'Copy' in f and c_type in f]
+    # report_filenames = [report_filenames[0]]
 
-if process_flow['get_priors']:
-    print('get_priors...')
-    with open(results_path + 'ltv_class.pkl', 'rb') as input:
-        the_class = pickle.load(input)
-    the_class.define_priors(n_bins=20)
-    the_class.update_priors()
-    with open(results_path + 'ltv_class.pkl', 'wb') as output:
-        pickle.dump(the_class, output, pickle.HIGHEST_PROTOCOL)
+    if process_flow['get_stats']:
+        print('get_stats...')
+        the_class = LtvClass(filename=past_filename, target_periods=[7,30,90], max_count=100000)
+        the_class.get_stats()
+        with open(results_path + 'ltv_class.pkl', 'wb') as output:
+            pickle.dump(the_class, output, pickle.HIGHEST_PROTOCOL)
 
-if process_flow['update_posteriors']:
-    print('update_posteriors...')
-    with open(results_path + 'ltv_class.pkl', 'rb') as input:
-        the_class = pickle.load(input)
-    the_class.get_reports_data(['FTBs_less10_week'])
-    the_class.get_users()
-    the_class.update_posteriors()
-    with open(results_path + 'ltv_class.pkl', 'wb') as output:
-        pickle.dump(the_class, output, pickle.HIGHEST_PROTOCOL)
+    if process_flow['get_priors']:
+        print('get_priors...')
+        with open(results_path + 'ltv_class.pkl', 'rb') as input:
+            the_class = pickle.load(input)
+        the_class.define_priors(n_bins=100)
+        the_class.update_priors()
+        with open(results_path + 'ltv_class.pkl', 'wb') as output:
+            pickle.dump(the_class, output, pickle.HIGHEST_PROTOCOL)
 
-if process_flow['calc_ltv']:
-    print('calc ltv...')
-    with open(results_path + 'ltv_class.pkl', 'rb') as input:
-        the_class = pickle.load(input)
-    the_class.get_ltv()
-    with open(results_path + 'ltv_class.pkl', 'wb') as output:
-        pickle.dump(the_class, output, pickle.HIGHEST_PROTOCOL)
+    if process_flow['update_posteriors']:
+        print('update_posteriors...')
+        with open(results_path + 'ltv_class.pkl', 'rb') as input:
+            the_class = pickle.load(input)
+        the_class.get_reports_data(report_filenames)
+        the_class.get_users(max_users=-1)
+        the_class.update_posteriors()
 
-print('loading results...')
-with open(results_path + 'ltv_class.pkl', 'rb') as input:
-    the_class = pickle.load(input)
-print('done!')
-for u in the_class.user_data.values():
-    print(u['ltv'])
+    if process_flow['calc_ltv']:
+        print('calc ltv...')
+        the_class.get_ltv()
 
-# for p in the_class.priors:
-#     print(np.log(p))
-#
-# print(the_class.target_periods)
-# generate prior distribution
+    if process_flow['update_ranking']:
+        print('update_ranking...')
+        with open(results_path + 'ltv_class.pkl', 'rb') as input:
+            the_class = pickle.load(input)
+        the_class.get_reports_data(report_filenames)
+        the_class.get_users(max_users=-1)
+        the_class.update_rankings()
+        the_class.get_ltv_ranking()
 
-# go over reports
-# update per ID the posterior per report
+    if process_flow['check_result']:
+        print('check result...')
+        the_class.check_result(present_filename)
+
+        print('saving data...')
+        with open(results_path + c_type + '_final_results.csv', 'w') as csv_file:
+            str_title = 'user_id, '
+
+            str_title += 'week_predicted, week_ground_truth, week_error, week_error_sq, week_correct_0, '
+            str_title += 'month_predicted, month_ground_truth, month_error, month_error_sq, month_correct_0, '
+            str_title += '3months_predicted, 3months_ground_truth, 3months_error, 3months_error_sq, 3months_correct_0'
+
+            csv_file.write(str_title + '\n')
+            for ids, d in the_class.final_results.items():
+                csv_line = ids
+                for i in range(3):
+                    csv_line += ',' + str(d['predicted'][i])
+                    csv_line += ',' + str(d['ground_truth'][i])
+                    csv_line += ',' + str(d['predicted'][i] - d['ground_truth'][i])
+                    csv_line += ',' + str(np.square(d['predicted'][i] - d['ground_truth'][i]))
+                    if (d['predicted'][i] == 0 and d['ground_truth'][i] == 0) or \
+                        (d['predicted'][i] > 0 and d['ground_truth'][i] > 0):
+                        csv_line += ', 1'
+                    else:
+                        csv_line += ', 0'
+                csv_file.write(csv_line + '\n')
